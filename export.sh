25ea1202ba1ca92e70599090c112c269f0ec01c6
Changooo <cgl1234321@gmail.com>
2025-11-22 19:47:24 +0900

    a

--- export.sh
+++ export.sh
@@ -1,1 +1,9 @@
-echo 'export PATH="$HOME/hire/src:$PATH"' >> ~/.bashrc
+#!/bin/bash
+# Add hire/src to PATH if not already present
+if ! grep -q 'export PATH="$HOME/hire/src:$PATH"' ~/.bashrc; then
+    echo 'export PATH="$HOME/hire/src:$PATH"' >> ~/.bashrc
+    echo "Added hire/src to PATH in ~/.bashrc"
+    echo "Run 'source ~/.bashrc' to apply changes to current shell"
+else
+    echo "hire/src already in PATH in ~/.bashrc"
+fi
